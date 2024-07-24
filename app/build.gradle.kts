2cae47074d195bb2738c84eb9a1a143740dc1f2a
Abdourahamane Boinaidi <abdourahamane.boinaidi@infomaniak.com>
2024-07-24 14:56:29 +0200

    Remove parcelize

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,7 +2,6 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.compose.compiler)
-    id("kotlin-parcelize")
     kotlin("plugin.serialization") version libs.versions.kotlin
 }
 
