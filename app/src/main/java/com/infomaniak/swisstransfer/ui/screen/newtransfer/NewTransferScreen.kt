96aaaa80b6a1fa790f3cbeba63d52bc978e6faea
Abdourahamane Boinaidi <abdourahamane.boinaidi@infomaniak.com>
2024-07-26 10:22:25 +0200

    core: Init NewTransferNavHost and NewTransferSreen

---
+++ app/src/main/java/com/infomaniak/swisstransfer/ui/screen/newtransfer/NewTransferScreen.kt
@@ -0,0 +1,46 @@
+/*
+ * Infomaniak SwissTransfer - Android
+ * Copyright (C) 2024 Infomaniak Network SA
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.infomaniak.swisstransfer.ui.screen.newtransfer
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.rememberNavController
+import com.infomaniak.swisstransfer.ui.theme.SwissTransferTheme
+import com.infomaniak.swisstransfer.ui.utils.PreviewMobile
+import com.infomaniak.swisstransfer.ui.utils.PreviewTablet
+
+@Composable
+fun NewTransferScreen() {
+    val navController = rememberNavController()
+
+    Scaffold { paddingValues ->
+        NewTransferNavHost(navController, modifier = Modifier.padding(paddingValues))
+    }
+}
+
+@PreviewMobile
+@PreviewTablet
+@Composable
+private fun NewTransferPreview() {
+    SwissTransferTheme {
+        NewTransferScreen()
+    }
+}
