ec832e7823248ceb4a3ba2e39138688aae7fa584
Gibran Chevalley <gibran.chevalley@infomaniak.com>
2024-08-12 15:11:06 +0200

    Add shapes for the corner radiuses
    From a quick look at the specs, this seems to be the commonly used values. Mainly 16dp and 50%, but there also are some rare occurrences of 4dp and 8dp

--- app/src/main/java/com/infomaniak/swisstransfer/ui/theme/Shapes.kt
+++ app/src/main/java/com/infomaniak/swisstransfer/ui/theme/Shapes.kt
@@ -23,5 +23,8 @@ import androidx.compose.material3.Shapes
 import androidx.compose.ui.unit.dp
 
 val Shapes = Shapes(
+    extraSmall = RoundedCornerShape(4.dp),
+    small = RoundedCornerShape(8.dp),
     medium = RoundedCornerShape(16.dp),
+    extraLarge = RoundedCornerShape(50),
 )
