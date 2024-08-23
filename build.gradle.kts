e3bf3e13497f52e67b792e732d46c9e0de2d5840
Abdourahamane Boinaidi <abdourahamane.boinaidi@infomaniak.com>
2024-08-23 16:43:49 +0200

    Add hilt + support compose navigation

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,4 +10,6 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.compose.compiler) apply false
+    alias(libs.plugins.kapt) apply false
+    alias(libs.plugins.hilt) apply false
 }
