636f0c0a88a36237c95123f3cd250b4cc49772fe
Kevin Boulongne <kevin.boulongne@infomaniak.com>
2024-09-04 11:04:51 +0200

    Extract ReceivedEmptyFab to its own component file

--- app/src/main/java/com/infomaniak/swisstransfer/ui/screen/main/received/ReceivedScreen.kt
+++ app/src/main/java/com/infomaniak/swisstransfer/ui/screen/main/received/ReceivedScreen.kt
@@ -17,27 +17,19 @@
  */
 package com.infomaniak.swisstransfer.ui.screen.main.received
 
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.material3.adaptive.navigationsuite.NavigationSuiteType
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
-import androidx.constraintlayout.compose.ConstraintLayout
 import com.infomaniak.swisstransfer.R
 import com.infomaniak.swisstransfer.ui.components.BrandTobAppBar
 import com.infomaniak.swisstransfer.ui.components.EmptyState
-import com.infomaniak.swisstransfer.ui.components.NewTransferFab
-import com.infomaniak.swisstransfer.ui.components.NewTransferFabType
 import com.infomaniak.swisstransfer.ui.images.AppImages.AppIllus
-import com.infomaniak.swisstransfer.ui.images.illus.ArrowRightCurved
 import com.infomaniak.swisstransfer.ui.images.illus.MascotSearching
 import com.infomaniak.swisstransfer.ui.screen.main.LocalNavType
-import com.infomaniak.swisstransfer.ui.theme.Margin
+import com.infomaniak.swisstransfer.ui.screen.main.received.components.ReceivedEmptyFab
 import com.infomaniak.swisstransfer.ui.theme.SwissTransferTheme
 import com.infomaniak.swisstransfer.ui.utils.PreviewMobile
 import com.infomaniak.swisstransfer.ui.utils.PreviewTablet
@@ -53,39 +45,7 @@ fun ReceivedScreen(navigateToDetails: (transferId: Int) -> Unit) {
 private fun ReceivedScreen(navType: NavigationSuiteType) {
     Scaffold(
         topBar = { BrandTobAppBar() },
-        floatingActionButton = {
-            ConstraintLayout {
-                val (text, icon, fab) = createRefs()
-
-                Text(
-                    text = LocalContext.current.getString(R.string.firstTransferDescription),
-                    style = SwissTransferTheme.typography.bodyRegular,
-                    color = SwissTransferTheme.colors.secondaryTextColor,
-                    modifier = Modifier
-                        .padding(PaddingValues(top = Margin.XLarge))
-                        .constrainAs(text) {
-                            bottom.linkTo(icon.top, Margin.Small)
-                            end.linkTo(icon.end, Margin.Small)
-                        },
-                )
-
-                Icon(
-                    modifier = Modifier
-                        .constrainAs(icon) {
-                            top.linkTo(fab.top)
-                            bottom.linkTo(fab.bottom, Margin.Small)
-                            end.linkTo(fab.start, Margin.Medium)
-                        },
-                    imageVector = AppIllus.ArrowRightCurved,
-                    contentDescription = null,
-                )
-
-                NewTransferFab(
-                    modifier = Modifier.constrainAs(fab) { },
-                    newTransferFabType = NewTransferFabType.BOTTOM_BAR,
-                )
-            }
-        },
+        floatingActionButton = { ReceivedEmptyFab() },
     ) { contentPadding ->
 
         EmptyState(
