3fb32549a1cc5a5cf0299b58ab8f43e9f1a6e868
Gibran Chevalley <gibran.chevalley@infomaniak.com>
2024-10-17 15:18:32 +0200

    Compute importation progress

--- app/src/main/java/com/infomaniak/swisstransfer/ui/screen/newtransfer/importfiles/ImportFilesScreen.kt
+++ app/src/main/java/com/infomaniak/swisstransfer/ui/screen/newtransfer/importfiles/ImportFilesScreen.kt
@@ -48,9 +48,11 @@ fun ImportFilesScreen(
 ) {
     val files by newTransferViewModel.filesDebounced.collectAsStateWithLifecycle()
     val filesToImportCount by newTransferViewModel.filesToImportCount.collectAsStateWithLifecycle()
+    val currentSessionTotalUploadedFiles by newTransferViewModel.currentSessionTotalUploadedFiles.collectAsStateWithLifecycle()
     ImportFilesScreen(
         files = { files },
         filesToImportCount = { filesToImportCount },
+        currentSessionTotalUploadedFiles = { currentSessionTotalUploadedFiles },
         removeFileByUid = newTransferViewModel::removeFileByUid,
         addFiles = newTransferViewModel::addFiles,
         closeActivity = closeActivity,
@@ -62,6 +64,7 @@ fun ImportFilesScreen(
 private fun ImportFilesScreen(
     files: () -> List<FileUi>,
     filesToImportCount: () -> Int,
+    currentSessionTotalUploadedFiles: () -> Int,
     removeFileByUid: (uid: String) -> Unit,
     addFiles: (List<Uri>) -> Unit,
     closeActivity: () -> Unit,
@@ -76,7 +79,9 @@ private fun ImportFilesScreen(
         val spaceLeft = (TOTAL_FILE_SIZE - usedSpace).coerceAtLeast(0)
         getHumanReadableSize(context, spaceLeft)
     }
-    val isSendButtonEnabled by remember { derivedStateOf { importedFiles.isNotEmpty() && filesToImportCount() == 0 } }
+    val isSendButtonEnabled by remember { derivedStateOf { importedFiles.isNotEmpty() } }
+    val isImporting by remember { derivedStateOf { filesToImportCount() > 0 } }
+    val importProgress by remember { derivedStateOf { 1 - (filesToImportCount().toFloat() / currentSessionTotalUploadedFiles()) } }
 
     val filePickerLauncher = rememberLauncherForActivityResult(
         contract = ActivityResultContracts.OpenMultipleDocuments()
@@ -93,11 +98,17 @@ private fun ImportFilesScreen(
             )
         },
         topButton = { modifier ->
+            val progress: (() -> Float)? = if (isImporting) {
+                { importProgress }
+            } else {
+                null
+            }
+
             LargeButton(
                 modifier = modifier,
                 titleRes = R.string.transferSendButton,
                 style = ButtonType.PRIMARY,
-                enabled = { isSendButtonEnabled },
+                progress = progress,
                 onClick = { /*TODO*/ },
             )
         },
@@ -134,6 +145,7 @@ private fun ImportFilesScreenPreview(@PreviewParameter(FileUiListPreviewParamete
         ImportFilesScreen(
             files = { files },
             filesToImportCount = { 0 },
+            currentSessionTotalUploadedFiles = { 0 },
             removeFileByUid = {},
             addFiles = {},
             closeActivity = {},
