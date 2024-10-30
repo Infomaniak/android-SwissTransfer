f3e585650b4cb954f0ac2ef7ae71ab968ace7c28
Gibran Chevalley <gibran.chevalley@infomaniak.com>
2024-10-30 15:22:59 +0100

    Center bottom sheet titles if it needs to wrap

--- app/src/main/java/com/infomaniak/swisstransfer/ui/components/SwissTransferBottomSheet.kt
+++ app/src/main/java/com/infomaniak/swisstransfer/ui/components/SwissTransferBottomSheet.kt
@@ -34,6 +34,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.infomaniak.swisstransfer.R
@@ -92,6 +93,7 @@ private fun BottomSheetContent(
             text = stringResource(titleRes),
             style = SwissTransferTheme.typography.bodyMedium,
             color = SwissTransferTheme.colors.primaryTextColor,
+            textAlign = TextAlign.Center,
         )
         Spacer(modifier = Modifier.height(Margin.Large))
 
