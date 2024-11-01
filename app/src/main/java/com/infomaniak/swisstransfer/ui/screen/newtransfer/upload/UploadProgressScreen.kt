7a0b29657ea9ac5b2a3a02f8e8dae715c0295ce6
Gibran Chevalley <gibran.chevalley@infomaniak.com>
2024-11-01 13:03:57 +0100

    Extract components and enum to their own files

--- app/src/main/java/com/infomaniak/swisstransfer/ui/screen/newtransfer/upload/UploadProgressScreen.kt
+++ app/src/main/java/com/infomaniak/swisstransfer/ui/screen/newtransfer/upload/UploadProgressScreen.kt
@@ -17,41 +17,31 @@
  */
 package com.infomaniak.swisstransfer.ui.screen.newtransfer.upload
 
-import androidx.annotation.StringRes
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.AnnotatedString
-import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.buildAnnotatedString
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.Dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.infomaniak.swisstransfer.R
 import com.infomaniak.swisstransfer.ui.components.*
 import com.infomaniak.swisstransfer.ui.images.AppImages.AppIllus
-import com.infomaniak.swisstransfer.ui.images.ThemedImage
-import com.infomaniak.swisstransfer.ui.images.illus.uploadAd.MetallicSafe
-import com.infomaniak.swisstransfer.ui.images.illus.uploadAd.MountainGondola
-import com.infomaniak.swisstransfer.ui.images.illus.uploadAd.SwissWithFlag
 import com.infomaniak.swisstransfer.ui.images.illus.uploadCancelBottomSheet.RedCrossPaperPlanes
 import com.infomaniak.swisstransfer.ui.screen.newtransfer.NewTransferViewModel
-import com.infomaniak.swisstransfer.ui.theme.Dimens
+import com.infomaniak.swisstransfer.ui.screen.newtransfer.upload.components.AdHeader
+import com.infomaniak.swisstransfer.ui.screen.newtransfer.upload.components.Progress
 import com.infomaniak.swisstransfer.ui.theme.Margin
 import com.infomaniak.swisstransfer.ui.theme.SwissTransferTheme
-import com.infomaniak.swisstransfer.ui.utils.HumanReadableSizeUtils
 import com.infomaniak.swisstransfer.ui.utils.PreviewAllWindows
-import java.util.Locale
 
 @Composable
 fun UploadProgressScreen(newTransferViewModel: NewTransferViewModel = hiltViewModel<NewTransferViewModel>()) {
@@ -89,29 +79,7 @@ private fun UploadProgressScreen(
             modifier = Modifier.fillMaxSize(),
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
-            Column(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .weight(1f)
-                    .verticalScroll(rememberScrollState()),
-                horizontalAlignment = Alignment.CenterHorizontally,
-            ) {
-                Spacer(modifier = Modifier.height(Margin.Giant))
-                Text(stringResource(R.string.uploadSuccessTitle), style = SwissTransferTheme.typography.bodyMedium)
-                Spacer(modifier = Modifier.height(Margin.Huge))
-                Text(
-                    text = adScreenType.description(),
-                    modifier = Modifier.widthIn(max = Dimens.DescriptionWidth),
-                    style = SwissTransferTheme.typography.specificLight18,
-                    textAlign = TextAlign.Center,
-                )
-
-                WeightOneSpacer(minHeight = Margin.Medium)
-
-                Image(imageVector = adScreenType.illustration.image(), contentDescription = null)
-
-                WeightOneSpacer(minHeight = Margin.Medium)
-            }
+            AdHeader(adScreenType)
 
             Spacer(modifier = Modifier.height(Margin.Medium))
             Text(stringResource(R.string.uploadSuccessTransferInProgress))
@@ -146,94 +114,6 @@ private fun CancelUploadBottomSheet(onCancel: () -> Unit, closeButtonSheet: () -
         },
         onDismissRequest = closeButtonSheet,
     )
-}
-
-@Composable
-private fun ColumnScope.WeightOneSpacer(minHeight: Dp) {
-    Spacer(modifier = Modifier.height(minHeight))
-    Spacer(modifier = Modifier.weight(1f))
-}
-
-@Composable
-fun Progress(uploadedSizeInBytes: () -> Long, totalSizeInBytes: () -> Long) {
-    Row {
-        Percentage(uploadedSizeInBytes, totalSizeInBytes)
-        Text(" - ")
-        UploadedSize(uploadedSizeInBytes)
-        Text(" / ")
-        TotalSize(totalSizeInBytes)
-    }
-}
-
-@Composable
-private fun Percentage(uploadedSizeInBytes: () -> Long, totalSizeInBytes: () -> Long) {
-    val percentageNoDecimals by remember {
-        derivedStateOf {
-            val percentage = (uploadedSizeInBytes().toFloat() / totalSizeInBytes())
-            String.format(Locale.getDefault(), "%d", (percentage * 100).toInt())
-        }
-    }
-
-    Text("$percentageNoDecimals%")
-}
-
-@Composable
-private fun UploadedSize(uploadedSizeInBytes: () -> Long) {
-    val context = LocalContext.current
-    val humanReadableSize by remember {
-        derivedStateOf { HumanReadableSizeUtils.getHumanReadableSize(context, uploadedSizeInBytes()) }
-    }
-
-    Text(humanReadableSize)
-}
-
-@Composable
-private fun TotalSize(totalSizeInBytes: () -> Long) {
-    val context = LocalContext.current
-    val humanReadableTotalSize by remember {
-        derivedStateOf { HumanReadableSizeUtils.getHumanReadableSize(context, totalSizeInBytes()) }
-    }
-
-    Text(humanReadableTotalSize)
-}
-
-
-enum class UploadProgressAdType(
-    @StringRes private val descriptionTemplate: Int,
-    @StringRes private val descriptionAccentuatedPart: Int,
-    val illustration: ThemedImage,
-) {
-    INDEPENDENCE(
-        R.string.uploadSuccessDescriptionTemplateIndependence,
-        R.string.uploadSuccessDescriptionArgumentIndependence,
-        AppIllus.SwissWithFlag,
-    ),
-    ENERGY(
-        R.string.uploadSuccessDescriptionTemplateEnergy,
-        R.string.uploadSuccessDescriptionArgumentEnergy,
-        AppIllus.MountainGondola,
-    ),
-    CONFIDENTIALITY(
-        R.string.uploadSuccessDescriptionTemplateConfidentiality,
-        R.string.uploadSuccessDescriptionArgumentConfidentiality,
-        AppIllus.MetallicSafe,
-    );
-
-    @Composable
-    fun description(): AnnotatedString {
-        val template = stringResource(descriptionTemplate)
-        val templateArgument = stringResource(descriptionAccentuatedPart)
-
-        val description = String.format(template, templateArgument)
-
-        val startIndex = description.indexOf(templateArgument)
-        val endIndex = startIndex + templateArgument.length
-
-        return buildAnnotatedString {
-            append(description)
-            addStyle(style = SpanStyle(fontWeight = FontWeight.Bold), start = startIndex, end = endIndex)
-        }
-    }
 }
 
 @PreviewAllWindows
