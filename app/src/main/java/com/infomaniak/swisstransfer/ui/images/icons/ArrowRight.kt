e99e3a97aa5af4542444f1b8c85c899fed6d2829
Gibran Chevalley <gibran.chevalley@infomaniak.com>
2024-11-22 16:42:56 +0100

    feat: Animate the onboarding button

---
+++ app/src/main/java/com/infomaniak/swisstransfer/ui/images/icons/ArrowRight.kt
@@ -0,0 +1,60 @@
+package com.infomaniak.swisstransfer.ui.images.icons
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.PathFillType.Companion.NonZero
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.graphics.vector.ImageVector.Builder
+import androidx.compose.ui.graphics.vector.path
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.infomaniak.swisstransfer.ui.images.AppImages
+import com.infomaniak.swisstransfer.ui.images.AppImages.AppIcons
+import androidx.compose.ui.graphics.StrokeCap.Companion.Round as strokeCapRound
+import androidx.compose.ui.graphics.StrokeJoin.Companion.Round as strokeJoinRound
+
+val AppIcons.ArrowRight: ImageVector
+    get() {
+        if (_arrowRight != null) {
+            return _arrowRight!!
+        }
+        _arrowRight = Builder(
+            name = "ArrowRight", defaultWidth = 24.0.dp, defaultHeight = 24.0.dp, viewportWidth = 24.0f, viewportHeight = 24.0f
+        ).apply {
+            path(
+                fill = null,
+                stroke = SolidColor(Color(0xFF9F9F9F)),
+                strokeLineWidth = 2.5f,
+                strokeLineCap = strokeCapRound,
+                strokeLineJoin = strokeJoinRound,
+                strokeLineMiter = 4.0f,
+                pathFillType = NonZero
+            ) {
+                moveTo(1.25f, 12.0f)
+                horizontalLineToRelative(21.5f)
+                moveToRelative(-8.5f, 8.0f)
+                lineToRelative(8.5f, -8.0f)
+                lineToRelative(-8.5f, -8.0f)
+            }
+        }.build()
+        return _arrowRight!!
+    }
+
+private var _arrowRight: ImageVector? = null
+
+@Preview
+@Composable
+private fun Preview() {
+    Box {
+        Image(
+            imageVector = AppIcons.ArrowRight,
+            contentDescription = null,
+            modifier = Modifier.size(AppImages.previewSize)
+        )
+    }
+}
