50216e4d865b4ff808c1b298ecfcc7726cebd9c3
Fabian Devel <fabian.devel@infomaniak.com>
2024-11-26 09:36:00 +0100

    chore(QrCode): Reimport icon without aggressive optimization for api < 30

--- app/src/main/java/com/infomaniak/swisstransfer/ui/images/icons/QrCode.kt
+++ app/src/main/java/com/infomaniak/swisstransfer/ui/images/icons/QrCode.kt
@@ -27,7 +27,6 @@ import androidx.compose.ui.graphics.PathFillType.Companion.NonZero
 import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.graphics.vector.ImageVector.Builder
-import androidx.compose.ui.graphics.vector.group
 import androidx.compose.ui.graphics.vector.path
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -48,66 +47,63 @@ val AppIcons.QrCode: ImageVector
             viewportWidth = 24.0f,
             viewportHeight = 25.0f,
         ).apply {
-            group {
-                path(
-                    fill = null,
-                    stroke = SolidColor(Color(0xFF9F9F9F)),
-                    strokeLineWidth = 1.5f,
-                    strokeLineCap = strokeCapRound,
-                    strokeLineJoin = strokeJoinRound,
-                    strokeLineMiter = 4.0f,
-                    pathFillType = NonZero,
-                ) {
-                    moveTo(5.25f, 6.033f)
-                    horizontalLineToRelative(3.0f)
-                    verticalLineToRelative(3.0f)
-                    horizontalLineToRelative(-3.0f)
-                    close()
-                    moveToRelative(0.0f, 10.5f)
-                    horizontalLineToRelative(3.0f)
-                    verticalLineToRelative(3.0f)
-                    horizontalLineToRelative(-3.0f)
-                    close()
-                    moveToRelative(10.5f, -10.5f)
-                    horizontalLineToRelative(3.0f)
-                    verticalLineToRelative(3.0f)
-                    horizontalLineToRelative(-3.0f)
-                    close()
-                    moveToRelative(-10.5f, 7.5f)
-                    horizontalLineToRelative(6.0f)
-                    verticalLineToRelative(1.5f)
-                    moveToRelative(3.0f, 0.0f)
-                    verticalLineToRelative(4.5f)
-                    horizontalLineToRelative(4.5f)
-                    verticalLineToRelative(-4.5f)
-                    horizontalLineToRelative(-1.5f)
-                    moveToRelative(-6.0f, 3.0f)
-                    verticalLineToRelative(1.5f)
-                    moveToRelative(0.0f, -13.5f)
-                    verticalLineToRelative(4.5f)
-                    horizontalLineToRelative(1.5f)
-                    moveToRelative(3.0f, 1.5f)
-                    horizontalLineToRelative(3.0f)
-                    moveToRelative(-18.0f, -5.25f)
-                    verticalLineToRelative(-3.75f)
-                    arcToRelative(1.5f, 1.5f, 0.0f, false, true, 1.5f, -1.5f)
-                    horizontalLineTo(6.0f)
-                    moveToRelative(12.0f, 0.0f)
-                    horizontalLineToRelative(3.75f)
-                    arcToRelative(1.5f, 1.5f, 0.0f, false, true, 1.5f, 1.5f)
-                    verticalLineToRelative(3.75f)
-                    moveToRelative(0.0f, 12.0f)
-                    verticalLineToRelative(3.75f)
-                    arcToRelative(1.5f, 1.5f, 0.0f, false, true, -1.5f, 1.5f)
-                    horizontalLineTo(18.0f)
-                    moveToRelative(-12.0f, 0.0f)
-                    horizontalLineTo(2.25f)
-                    arcToRelative(1.5f, 1.5f, 0.0f, false, true, -1.5f, -1.5f)
-                    verticalLineToRelative(-3.75f)
-                }
+            path(
+                stroke = SolidColor(Color(0xFF9F9F9F)),
+                strokeLineWidth = 1.5f,
+                strokeLineCap = strokeCapRound,
+                strokeLineJoin = strokeJoinRound,
+                strokeLineMiter = 4.0f,
+                pathFillType = NonZero,
+            ) {
+                moveTo(5.25f, 5.866f)
+                horizontalLineToRelative(3.0f)
+                verticalLineToRelative(3.0f)
+                horizontalLineToRelative(-3.0f)
+                close()
+                moveTo(5.25f, 16.366f)
+                horizontalLineToRelative(3.0f)
+                verticalLineToRelative(3.0f)
+                horizontalLineToRelative(-3.0f)
+                close()
+                moveTo(15.75f, 5.866f)
+                horizontalLineToRelative(3.0f)
+                verticalLineToRelative(3.0f)
+                horizontalLineToRelative(-3.0f)
+                close()
+                moveTo(5.25f, 13.366f)
+                horizontalLineToRelative(6.0f)
+                verticalLineToRelative(1.5f)
+                moveTo(14.25f, 14.866f)
+                verticalLineToRelative(4.5f)
+                horizontalLineToRelative(4.5f)
+                verticalLineToRelative(-4.5f)
+                horizontalLineToRelative(-1.5f)
+                moveTo(11.25f, 17.866f)
+                verticalLineToRelative(1.5f)
+                moveTo(11.25f, 5.866f)
+                verticalLineToRelative(4.5f)
+                horizontalLineToRelative(1.5f)
+                moveTo(15.75f, 11.866f)
+                horizontalLineToRelative(3.0f)
+                moveTo(0.75f, 6.616f)
+                verticalLineToRelative(-3.75f)
+                arcToRelative(1.5f, 1.5f, 0.0f, false, true, 1.5f, -1.5f)
+                horizontalLineTo(6.0f)
+                moveTo(18.0f, 1.366f)
+                horizontalLineToRelative(3.75f)
+                arcToRelative(1.5f, 1.5f, 0.0f, false, true, 1.5f, 1.5f)
+                verticalLineToRelative(3.75f)
+                moveTo(23.25f, 18.616f)
+                verticalLineToRelative(3.75f)
+                arcToRelative(1.5f, 1.5f, 0.0f, false, true, -1.5f, 1.5f)
+                horizontalLineTo(18.0f)
+                moveTo(6.0f, 23.866f)
+                horizontalLineTo(2.25f)
+                arcToRelative(1.5f, 1.5f, 0.0f, false, true, -1.5f, -1.5f)
+                verticalLineToRelative(-3.75f)
             }
-        }.build()
-
+        }
+            .build()
         return _qrCode!!
     }
 
