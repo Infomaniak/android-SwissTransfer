72475242386b48a7e1583f21d7c2ccfb6661b86a
Gibran Chevalley <gibran.chevalley@infomaniak.com>
2024-11-26 14:59:33 +0100

    feat: Import and use illustrations

---
+++ app/src/main/java/com/infomaniak/swisstransfer/ui/images/illus/onboarding/TwoPadlocksIntertwinedStars.kt
@@ -0,0 +1,63 @@
+/*
+ * Infomaniak SwissTransfer - Android
+ * Copyright (C) 2024 Infomaniak Network SA
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.infomaniak.swisstransfer.ui.images.illus.onboarding
+
+
+import android.content.res.Configuration
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.tooling.preview.Preview
+import com.infomaniak.swisstransfer.ui.images.AppImages
+import com.infomaniak.swisstransfer.ui.images.AppImages.AppIllus
+import com.infomaniak.swisstransfer.ui.images.ThemedImage
+import com.infomaniak.swisstransfer.ui.theme.SwissTransferTheme
+
+val AppIllus.TwoPadlocksIntertwinedStars: ThemedImage
+    get() = _twoPadlocksIntertwinedStars ?: object : ThemedImage {
+        override val light = AppIllus.TwoPadlocksIntertwinedStarsLight
+        override val dark = AppIllus.TwoPadlocksIntertwinedStarsDark
+    }.also { _twoPadlocksIntertwinedStars = it }
+
+private var _twoPadlocksIntertwinedStars: ThemedImage? = null
+
+@Preview(name = "Light")
+@Preview(name = "Dark", uiMode = Configuration.UI_MODE_NIGHT_YES or Configuration.UI_MODE_TYPE_NORMAL)
+@Composable
+private fun Preview() {
+    SwissTransferTheme {
+        Surface {
+            val imageVector = AppIllus.TwoPadlocksIntertwinedStars.image()
+            Box {
+                Image(
+                    imageVector = imageVector,
+                    contentDescription = null,
+                    contentScale = ContentScale.FillBounds,
+                    modifier = Modifier
+                        .size(AppImages.previewSize)
+                        .aspectRatio(imageVector.viewportWidth / imageVector.viewportHeight)
+                )
+            }
+        }
+    }
+}
