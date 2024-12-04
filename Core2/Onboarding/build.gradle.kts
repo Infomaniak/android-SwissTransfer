c6b936dc0df7b55d6a0e3f5655969e37a134e9d7
Gibran Chevalley <gibran.chevalley@infomaniak.com>
2024-12-04 16:06:31 +0100

    refactor: Introduce compose BOM to handle compose dependencies versions

--- Core2/Onboarding/build.gradle.kts
+++ Core2/Onboarding/build.gradle.kts
@@ -41,11 +41,12 @@ android {
 }
 
 dependencies {
+    implementation(platform(core2.compose.bom))
     implementation(core2.androidx.core.ktx)
-    implementation(core2.androidx.material3.android)
-    implementation(core2.androidx.runtime.android)
-    implementation(core2.androidx.foundation.android)
-    implementation(core2.androidx.ui.tooling.preview.android)
+    implementation(core2.compose.foundation)
+    implementation(core2.compose.material3)
+    implementation(core2.compose.runtime)
+    implementation(core2.compose.ui.tooling.preview)
 
-    debugImplementation(core2.androidx.ui.tooling)
+    debugImplementation(core2.compose.ui.tooling)
 }
