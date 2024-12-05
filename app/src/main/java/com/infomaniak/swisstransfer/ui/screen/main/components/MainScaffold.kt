8e5b218459316769ee822d615cf1a6d26e069774
Vincent TE <vincent.te@infomaniak.com>
2024-12-05 15:48:36 +0100

    chore: Various fixes with top app bar and theme

--- app/src/main/java/com/infomaniak/swisstransfer/ui/screen/main/components/MainScaffold.kt
+++ app/src/main/java/com/infomaniak/swisstransfer/ui/screen/main/components/MainScaffold.kt
@@ -40,7 +40,7 @@ import com.infomaniak.swisstransfer.ui.utils.isWindowSmall
 fun MainScaffold(
     navController: NavHostController,
     currentDestination: MainNavigation,
-    largeWindowTopAppBar: @Composable () -> Unit = {},
+    windowTopAppBar: @Composable (isWindowLarge: Boolean) -> Unit = {},
     content: @Composable () -> Unit = {},
 ) {
     val navType by rememberNavType(currentDestination)
@@ -49,7 +49,7 @@ fun MainScaffold(
         navType = navType,
         currentDestination = currentDestination,
         navigateToSelectedItem = navController::navigateToSelectedItem,
-        largeWindowTopAppBar = largeWindowTopAppBar,
+        windowTopAppBar = windowTopAppBar,
         content = content
     )
 }
@@ -59,13 +59,13 @@ private fun MainScaffold(
     navType: NavigationSuiteType,
     currentDestination: MainNavigation,
     navigateToSelectedItem: (MainNavigation) -> Unit,
-    largeWindowTopAppBar: @Composable () -> Unit,
+    windowTopAppBar: @Composable (isWindowLarge: Boolean) -> Unit = {},
     content: @Composable () -> Unit,
 ) {
     val windowAdaptiveInfo = LocalWindowAdaptiveInfo.current
 
     Column {
-        if (windowAdaptiveInfo.isWindowLarge()) largeWindowTopAppBar()
+        windowTopAppBar(windowAdaptiveInfo.isWindowLarge())
         AppNavigationSuiteScaffold(navType, NavigationItem.entries, currentDestination, navigateToSelectedItem) {
             if (windowAdaptiveInfo.isWindowSmall()) {
                 Column {
@@ -129,7 +129,7 @@ private fun NavigationSmallWindowPreview() {
             currentDestination = MainNavigation.SentDestination,
             navigateToSelectedItem = {},
             navType = NavigationSuiteType.NavigationBar,
-            largeWindowTopAppBar = { BrandTopAppBar() },
+            windowTopAppBar = { isWindowLarge -> if (isWindowLarge) BrandTopAppBar() },
             content = {},
         )
     }
@@ -143,7 +143,7 @@ private fun NavigationLargeWindowPreview() {
             currentDestination = MainNavigation.SentDestination,
             navigateToSelectedItem = {},
             navType = NavigationSuiteType.NavigationRail,
-            largeWindowTopAppBar = { BrandTopAppBar() },
+            windowTopAppBar = { isWindowLarge -> if (isWindowLarge) BrandTopAppBar() },
             content = {},
         )
     }
