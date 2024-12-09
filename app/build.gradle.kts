8039acab9d9d428dcdd4053774c72cf71cd48e03
Gibran Chevalley <gibran.chevalley@infomaniak.com>
2024-12-09 16:19:49 +0100

    feat: Base the lock of the orientation on smalls screen on the compose boolean we already use

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -88,6 +88,7 @@ dependencies {
     implementation(project(":Core2:Matomo"))
     implementation(project(":Core2:Network"))
     implementation(project(":Core2:Onboarding"))
+    implementation(project(":Core2:compose:Core"))
     implementation(project(":FileTypes"))
     implementation(kotlin("reflect"))
 
