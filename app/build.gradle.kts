2c888c739239fbbaeb5f41ba5e5ce7978e17e482
Joris Bodin <joris.bodin@infomaniak.com>
2024-12-26 15:19:41 +0100

    chore: New version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         applicationId = "com.infomaniak.swisstransfer"
         minSdk = sharedMinSdk
         targetSdk = sharedCompileSdk
-        versionCode = 1_00_000_01
-        versionName = "1.0.0-Alpha3"
+        versionCode = 1_00_000_02
+        versionName = "1.0.0-Alpha4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
