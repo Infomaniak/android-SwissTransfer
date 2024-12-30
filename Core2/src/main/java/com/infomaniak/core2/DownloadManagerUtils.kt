94e82663d685e1ff82ec0e0b50146ef449a80214
Louis CAD <louis.cognault@infomaniak.com>
2024-12-30 17:40:42 +0100

    chore: Prepare live download status with DownloadManager extensions

--- Core2/src/main/java/com/infomaniak/core2/DownloadManagerUtils.kt
+++ Core2/src/main/java/com/infomaniak/core2/DownloadManagerUtils.kt
@@ -19,7 +19,6 @@ package com.infomaniak.core2
 
 import android.app.DownloadManager
 import android.app.DownloadManager.Request
-import android.content.Context
 import android.database.Cursor
 import android.net.Uri
 import android.os.Build.VERSION.SDK_INT
@@ -27,7 +26,8 @@ import android.os.Environment
 import com.infomaniak.core2.extensions.appName
 import com.infomaniak.core2.extensions.appVersionName
 import kotlinx.coroutines.*
-import splitties.init.appCtx
+import kotlinx.coroutines.flow.Flow
+import splitties.systemservices.downloadManager
 import splitties.toast.UnreliableToastApi
 import splitties.toast.toast
 
@@ -43,24 +43,48 @@ object DownloadManagerUtils {
         userAgent: String,
         extraHeaders: Iterable<Pair<String, String>> = emptySet(),
     ) {
+        val request = requestFor(
+            url = url,
+            name = name,
+            userAgent = userAgent,
+            extraHeaders = extraHeaders
+        )
+        val downloadReference = downloadManager.enqueue(request)
+        handleDownloadManagerErrors(downloadManager, downloadReference)
+    }
+
+    fun downloadFile(
+        url: String,
+        name: String,
+        userAgent: String,
+        extraHeaders: Iterable<Pair<String, String>> = emptySet(),
+    ): Flow<DownloadStatus> {
+        val request = requestFor(
+            url = url,
+            name = name,
+            userAgent = userAgent,
+            extraHeaders = extraHeaders
+        )
+        return downloadManager.downloadFile(request)
+    }
+
+    fun requestFor(
+        url: String,
+        name: String,
+        userAgent: String,
+        extraHeaders: Iterable<Pair<String, String>> = emptySet(),
+    ): Request = Request(Uri.parse(url)).also { r ->
+        r.setAllowedNetworkTypes(Request.NETWORK_WIFI or Request.NETWORK_MOBILE)
         val formattedName = name.replace(regexInvalidSystemChar, "_").replace("%", "_").let {
             // fix IllegalArgumentException only on Android 10 if multi dot
             if (SDK_INT == 29) it.replace(Regex("\\.{2,}"), ".") else it
         }
+        r.setTitle(formattedName)
+        r.setDescription(appName)
+        r.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, name)
+        r.addHeaders(userAgent, extraHeaders)
 
-        Request(Uri.parse(url)).apply {
-            setAllowedNetworkTypes(Request.NETWORK_WIFI or Request.NETWORK_MOBILE)
-            setTitle(formattedName)
-            setDescription(appName)
-            setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, name)
-            addHeaders(userAgent, extraHeaders)
-
-            setNotificationVisibility(Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
-
-            val downloadManager = appCtx.getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
-            val downloadReference = downloadManager.enqueue(this)
-            handleDownloadManagerErrors(downloadManager, downloadReference)
-        }
+        r.setNotificationVisibility(Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
     }
 
     private fun Request.addHeaders(userAgent: String, extraHeaders: Iterable<Pair<String, String>>) {
