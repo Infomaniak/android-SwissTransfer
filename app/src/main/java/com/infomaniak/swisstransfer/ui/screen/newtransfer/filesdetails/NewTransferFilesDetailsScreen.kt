69ed819868d8fc4b68544def039083404d452b85
Vincent TE <vincent.te@infomaniak.com>
2025-01-13 15:32:23 +0100

    chore: Use a LaunchedEffect to avoid a blinking white screen

--- app/src/main/java/com/infomaniak/swisstransfer/ui/screen/newtransfer/filesdetails/NewTransferFilesDetailsScreen.kt
+++ app/src/main/java/com/infomaniak/swisstransfer/ui/screen/newtransfer/filesdetails/NewTransferFilesDetailsScreen.kt
@@ -20,6 +20,7 @@ package com.infomaniak.swisstransfer.ui.screen.newtransfer.filesdetails
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -43,18 +44,17 @@ fun NewTransferFilesDetailsScreen(
 ) {
     val uiState by importFilesViewModel.filesDetailsUiState.collectAsStateWithLifecycle()
 
-    when (uiState) {
-        is FilesDetailsUiState.EmptyFiles -> navigateBack()
-        is FilesDetailsUiState.Success -> {
-            NewTransferFilesDetailsScreen(
-                files = (uiState as FilesDetailsUiState.Success).files,
-                withFilesSize = withFilesSize,
-                withSpaceLeft = withSpaceLeft,
-                onFileRemoved = getOnFileRemoveCallback(importFilesViewModel, withFileDelete),
-                navigateBack = navigateBack,
-            )
-        }
+    LaunchedEffect(uiState) {
+        if (uiState is FilesDetailsUiState.EmptyFiles) navigateBack()
     }
+
+    NewTransferFilesDetailsScreen(
+        files = (uiState as? FilesDetailsUiState.Success)?.files ?: emptyList(),
+        withFilesSize = withFilesSize,
+        withSpaceLeft = withSpaceLeft,
+        onFileRemoved = getOnFileRemoveCallback(importFilesViewModel, withFileDelete),
+        navigateBack = navigateBack,
+    )
 }
 
 private fun getOnFileRemoveCallback(
