55760e5ec83f5a2160750729a45304db9040cb09
Gibran Chevalley <gibran.chevalley@infomaniak.com>
2025-01-14 14:43:54 +0100

    chore: Remove unused variable

--- app/src/main/java/com/infomaniak/swisstransfer/ui/screen/newtransfer/SharedFilesManager.kt
+++ app/src/main/java/com/infomaniak/swisstransfer/ui/screen/newtransfer/SharedFilesManager.kt
@@ -1,8 +1,6 @@
 package com.infomaniak.swisstransfer.ui.screen.newtransfer
 
-import android.content.Context
 import android.net.Uri
-import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.asSharedFlow
@@ -10,9 +8,7 @@ import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
-class SharedFilesManager @Inject constructor(
-    @ApplicationContext private val appContext: Context,
-) {
+class SharedFilesManager @Inject constructor() {
     private val _sharedUris = MutableSharedFlow<List<Uri>?>(replay = 1)
     val sharedUris: SharedFlow<List<Uri>?> = _sharedUris.asSharedFlow()
 
