844faf82b414c3c1e1878299cb4298ebb8847269
Vincent TE <vincent.te@infomaniak.com>
2025-02-21 08:22:39 +0100

    feat: Add lottie animation when we upload files
    # Conflicts:
    #	app/src/main/java/com/infomaniak/swisstransfer/ui/screen/newtransfer/upload/components/AdHeader.kt

--- app/src/main/java/com/infomaniak/swisstransfer/ui/screen/newtransfer/upload/components/AdHeader.kt
+++ app/src/main/java/com/infomaniak/swisstransfer/ui/screen/newtransfer/upload/components/AdHeader.kt
@@ -37,6 +37,10 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.LottieConstants
+import com.airbnb.lottie.compose.rememberLottieComposition
 import com.infomaniak.core.compose.margin.Margin
 import com.infomaniak.swisstransfer.R
 import com.infomaniak.swisstransfer.ui.components.HighlightedText
@@ -54,6 +58,7 @@ fun ColumnScope.AdHeader(adScreenType: UploadProgressAdType) {
             .verticalScroll(rememberScrollState()),
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
+        val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(adScreenType.illustration))
         var isHighlighted by rememberSaveable { mutableStateOf(false) }
         LaunchedEffect(Unit) { isHighlighted = true }
 
@@ -77,7 +82,12 @@ fun ColumnScope.AdHeader(adScreenType: UploadProgressAdType) {
 
         WeightOneSpacer(minHeight = Margin.Medium)
 
-        Image(imageVector = adScreenType.illustration.image(), contentDescription = null)
+        LottieAnimation(
+            composition,
+            isPlaying = true,
+            reverseOnRepeat = adScreenType.shouldReverseOnRepeat,
+            iterations = LottieConstants.IterateForever,
+        )
 
         WeightOneSpacer(minHeight = Margin.Medium)
     }
