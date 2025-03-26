5d01d00845dc00fb42e965bbc3137e7615d9ac0d
Joris Bodin <joris.bodin@infomaniak.com>
2025-03-26 15:59:18 +0100

    chore: New version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,8 +34,8 @@ android {
         applicationId = "com.infomaniak.swisstransfer"
         minSdk = appMinSdk
         targetSdk = appCompileSdk
-        versionCode = 1_00_005_01
-        versionName = "1.0.5"
+        versionCode = 1_00_006_01
+        versionName = "1.0.6"
 
         setProperty("archivesBaseName", "swisstransfer-$versionName ($versionCode)")
 
