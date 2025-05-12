6e24e7b0573658175219c85d787ed93b880c978a
Louis CAD <louis.cognault@infomaniak.com>
2025-05-12 17:13:38 +0200

    chore(Version): 1.1.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,8 +31,8 @@ android {
         applicationId = "com.infomaniak.swisstransfer"
         minSdk = appMinSdk
         targetSdk = appCompileSdk
-        versionCode = 1_01_002_01
-        versionName = "1.1.2"
+        versionCode = 1_01_003_01
+        versionName = "1.1.3"
 
         setProperty("archivesBaseName", "swisstransfer-$versionName ($versionCode)")
 
