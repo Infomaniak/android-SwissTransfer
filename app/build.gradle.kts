07d5b643cc8d792255f4d21c4919e5fe1943fa50
Gibran Chevalley <gibran.chevalley@infomaniak.com>
2025-06-11 11:24:22 +0200

    chore: Remove kotlinCompilerExtensionVersion from the app's files as well

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -106,10 +106,6 @@ android {
         compose = true
     }
 
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.14"
-    }
-
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
