b7b4035eb4009ce41625893e41839b7fdfae5339
Kevin Boulongne <kevin.boulongne@infomaniak.com>
2025-07-08 06:54:49 +0200

    chore: Bump Libs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -157,7 +157,7 @@ dependencies {
     // Compose
     implementation(libs.androidx.activity.compose)
     implementation(platform(core.compose.bom))
-    implementation(libs.compose.foundation) // TODO: To be removed when compose 1.8.0 is stable
+    implementation(core.compose.foundation) // TODO: To be removed when compose 1.8.0 is stable
     implementation(core.compose.ui)
     implementation(libs.compose.ui.graphics)
     implementation(core.compose.material3)
