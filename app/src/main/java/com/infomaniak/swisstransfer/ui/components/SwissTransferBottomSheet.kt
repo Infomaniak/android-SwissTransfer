1eb9f68781745d4eb0349ff1d43b3b76e08df0a4
Jamy Bailly <jamy.bailly@infomaniak.com>
2025-09-29 14:45:55 +0200

    feat: Share BottomStickyButtonScaffold into Core

--- app/src/main/java/com/infomaniak/swisstransfer/ui/components/SwissTransferBottomSheet.kt
+++ app/src/main/java/com/infomaniak/swisstransfer/ui/components/SwissTransferBottomSheet.kt
@@ -42,6 +42,7 @@ import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import com.infomaniak.core.compose.bottomstickybuttonscaffold.DoubleButtonCombo
 import com.infomaniak.core.compose.margin.Margin
 import com.infomaniak.core.compose.preview.PreviewLightAndDark
 import com.infomaniak.swisstransfer.R
