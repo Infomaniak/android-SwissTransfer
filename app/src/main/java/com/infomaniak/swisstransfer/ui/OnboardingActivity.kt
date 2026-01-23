12b246a7d2794415d0f3cb9dbc0c6329e83412bd
Gibran Chevalley <gibran.chevalley@infomaniak.com>
2026-01-23 16:00:26 +0100

    refactor: Extract CrossAppLoginViewModel to its own file

--- app/src/main/java/com/infomaniak/swisstransfer/ui/OnboardingActivity.kt
+++ app/src/main/java/com/infomaniak/swisstransfer/ui/OnboardingActivity.kt
@@ -47,7 +47,7 @@ import com.infomaniak.core.network.ApiEnvironment
 import com.infomaniak.core.sentry.SentryLog
 import com.infomaniak.core.ui.compose.basics.LockScreenOrientation
 import com.infomaniak.lib.login.InfomaniakLogin
-import com.infomaniak.swisstransfer.BuildConfig
+import com.infomaniak.swisstransfer.ui.screen.onboarding.CrossAppLoginViewModel
 import com.infomaniak.swisstransfer.ui.screen.onboarding.OnboardingScreen
 import com.infomaniak.swisstransfer.ui.theme.LocalWindowAdaptiveInfo
 import com.infomaniak.swisstransfer.ui.theme.SwissTransferTheme
@@ -56,8 +56,6 @@ import com.infomaniak.swisstransfer.ui.utils.isWindowSmall
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
 import javax.inject.Inject
-
-class CrossAppLoginViewModel() : BaseCrossAppLoginViewModel(BuildConfig.APPLICATION_ID, BuildConfig.CLIENT_ID)
 
 @AndroidEntryPoint
 class OnboardingActivity : ComponentActivity() {
