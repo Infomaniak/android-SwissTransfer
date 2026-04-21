a7d315542716d7bf4e199d68b607af52f3d110cd
Aymeric MARIAUX <aymeric.mariaux@infomaniak.com>
2026-04-21 10:53:57 +0200

    chore: Remove useless imports

--- app/src/main/java/com/infomaniak/swisstransfer/ui/images/illus/screenshotBottomSheet/LightbulbDark.kt
+++ app/src/main/java/com/infomaniak/swisstransfer/ui/images/illus/screenshotBottomSheet/LightbulbDark.kt
@@ -19,7 +19,6 @@ package com.infomaniak.swisstransfer.ui.images.illus.screenshotBottomSheet
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -35,7 +34,6 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.infomaniak.swisstransfer.ui.images.AppImages
 import com.infomaniak.swisstransfer.ui.images.AppImages.AppIllus
-import com.infomaniak.swisstransfer.ui.images.illus.ghostPointingReport.GhostPointingReportDark
 
 val AppIllus.LightbulbDark: ImageVector
     get() {
