903de781b5ffbd45a4e9322eb41553b781db1ecd
Aymeric MARIAUX <aymeric.mariaux@infomaniak.com>
2026-05-26 08:26:19 +0200

    refactor: Fix rebase error

--- app/src/main/java/com/infomaniak/swisstransfer/ui/screen/main/transferdetails/TransferDetailsScreen.kt
+++ app/src/main/java/com/infomaniak/swisstransfer/ui/screen/main/transferdetails/TransferDetailsScreen.kt
@@ -1,6 +1,6 @@
 /*
  * Infomaniak SwissTransfer - Android
- * Copyright (C) 2024-2025 Infomaniak Network SA
+ * Copyright (C) 2024-2026 Infomaniak Network SA
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -427,7 +427,7 @@ private fun TransferDetailsScreen(
 }
 
 @Composable
-private fun getBottomBarPadding(): PaddingValues {
+fun getBottomBarPadding(): PaddingValues {
     return if (isWindowSmall()) {
         WindowInsets.navigationBars.asPaddingValues()
     } else {
