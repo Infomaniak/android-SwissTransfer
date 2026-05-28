a4139d8ad1c3f3db87289a87f69aa49934bd6aec
Aymeric MARIAUX <aymeric.mariaux@infomaniak.com>
2026-05-28 14:10:14 +0200

    refactor: Centralize MIME type and thumbnail generation to reduce duplication

--- app/src/main/java/com/infomaniak/swisstransfer/upload/TransferUploader.kt
+++ app/src/main/java/com/infomaniak/swisstransfer/upload/TransferUploader.kt
@@ -33,7 +33,7 @@ import com.infomaniak.multiplatform_swisstransfer.managers.TransferManager
 import com.infomaniak.multiplatform_swisstransfer.network.exceptions.NetworkException
 import com.infomaniak.swisstransfer.ui.screen.newtransfer.PickedFile
 import com.infomaniak.swisstransfer.ui.screen.newtransfer.ThumbnailsLocalStorage
-import com.infomaniak.swisstransfer.ui.utils.extractExtensionOrFallback
+import com.infomaniak.swisstransfer.upload.generateThumbnailIfNeeded
 import com.infomaniak.swisstransfer.upload.TransferUploaderV1.ChunkUploadStatus.DefinitelyComplete
 import com.infomaniak.swisstransfer.upload.TransferUploaderV1.ChunkUploadStatus.StartedOrComplete
 import com.infomaniak.swisstransfer.upload.UploadState.Ongoing.Uploading.Status
@@ -169,26 +169,16 @@ class TransferUploaderV1(
         val fileUUID: String = metadata.uuid
         SentryLog.i(TAG, "start upload file $fileUUID, with size ${metadata.pickedFile.size}")
 
-        if (!metadata.thumbnailSaved) {
-            val extension = targetFileUri.getMimeType().extractExtensionOrFallback(fallback = metadata.pickedFile.name)
-            thumbnailsLocalStorage.generateThumbnailFor(
-                fileUri = targetFileUri,
-                fileName = fileUUID,
-                extension = extension,
-            )
-            metadata.thumbnailSaved = true
-        }
+        generateThumbnailIfNeeded(
+            fileUri = targetFileUri,
+            fileName = fileUUID,
+            pickedFileName = metadata.pickedFile.name,
+            thumbnailSaved = metadata.thumbnailSaved,
+            thumbnailsLocalStorage = thumbnailsLocalStorage,
+            contentResolver = contentResolver,
+            onThumbnailGenerated = { metadata.thumbnailSaved = true },
+        )
         uploadFileInChunks(metadata = metadata)
-    }
-
-    private fun Uri.getMimeType(): String? {
-        return when (scheme) {
-            ContentResolver.SCHEME_CONTENT -> contentResolver.getType(this)
-            ContentResolver.SCHEME_FILE -> MimeTypeMap.getSingleton().getMimeTypeFromExtension(
-                MimeTypeMap.getFileExtensionFromUrl(toString()).lowercase()
-            )
-            else -> null
-        }
     }
 
     private suspend fun syncFileChunksUploadStatuses(metadata: FileToUploadMetaData, totalChunks: Int) = coroutineScope {
---
+++ app/src/main/java/com/infomaniak/swisstransfer/upload/UploadCommon.kt
@@ -0,0 +1,55 @@
+/*
+ * Infomaniak SwissTransfer - Android
+ * Copyright (C) 2025-2026 Infomaniak Network SA
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.infomaniak.swisstransfer.upload
+
+import android.content.ContentResolver
+import android.net.Uri
+import android.webkit.MimeTypeMap
+import com.infomaniak.swisstransfer.ui.screen.newtransfer.ThumbnailsLocalStorage
+import com.infomaniak.swisstransfer.ui.utils.extractExtensionOrFallback
+
+fun Uri.getMimeType(contentResolver: ContentResolver): String? {
+    return when (scheme) {
+        ContentResolver.SCHEME_CONTENT -> contentResolver.getType(this)
+        ContentResolver.SCHEME_FILE -> MimeTypeMap.getSingleton().getMimeTypeFromExtension(
+            MimeTypeMap.getFileExtensionFromUrl(toString()).lowercase()
+        )
+        else -> null
+    }
+}
+
+suspend inline fun generateThumbnailIfNeeded(
+    fileUri: Uri,
+    fileName: String,
+    pickedFileName: String,
+    thumbnailSaved: Boolean,
+    thumbnailsLocalStorage: ThumbnailsLocalStorage,
+    contentResolver: ContentResolver,
+    crossinline onThumbnailGenerated: () -> Unit,
+) {
+    if (!thumbnailSaved) {
+        val extension = fileUri.getMimeType(contentResolver)
+            .extractExtensionOrFallback(fallback = pickedFileName)
+        thumbnailsLocalStorage.generateThumbnailFor(
+            fileUri = fileUri,
+            fileName = fileName,
+            extension = extension,
+        )
+        onThumbnailGenerated()
+    }
+}
