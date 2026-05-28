f975243ed404e2882e20d3e58490cb529935ed6b
Aymeric MARIAUX <aymeric.mariaux@infomaniak.com>
2026-05-28 08:55:05 +0200

    fix: Avoid making FileItem always clickable when no action is provided

--- app/src/main/java/com/infomaniak/swisstransfer/ui/components/FileItem.kt
+++ app/src/main/java/com/infomaniak/swisstransfer/ui/components/FileItem.kt
@@ -1,6 +1,6 @@
 /*
  * Infomaniak SwissTransfer - Android
- * Copyright (C) 2024 Infomaniak Network SA
+ * Copyright (C) 2024-2026 Infomaniak Network SA
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -164,10 +164,16 @@ private fun FileItemContent(
 private fun getCardModifier(onClick: (() -> Unit)? = null, onLongPress: (() -> Unit)? = null): Modifier {
     return Modifier
         .aspectRatio(164.0f / 152.0f)
-        .then(Modifier.combinedClickable(
-            onClick = onClick ?: {},
-            onLongClick = onLongPress ?: {}
-        ))
+        .then(
+            if (onClick != null || onLongPress != null) {
+                Modifier.combinedClickable(
+                    onClick = onClick ?: {},
+                    onLongClick = onLongPress
+                )
+            } else {
+                Modifier
+            }
+        )
 }
 
 @PreviewLightAndDark
